ebd7de3f20dec5b23496e922662563e9ee0bb8cd
ALAKNANDA96 <pundalku10@gmail.com>
2023-04-11 18:37:05 +0530

    my

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Form from './Form';
+import './index.css';
+import App from './App' 
+import Text from './Text' 
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    {/* <App />  */}
+    {/* <Text /> */}
+    <Form /> 
+  </React.StrictMode>
+);
